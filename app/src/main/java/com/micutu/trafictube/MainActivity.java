25985b9f257e25ddec3f3863d6e4cca047169039
micku7zu <micku7zu@gmail.com>
2016-09-22 20:46:35 +0300

    Add basic navigation drawer

--- app/src/main/java/com/micutu/trafictube/MainActivity.java
+++ app/src/main/java/com/micutu/trafictube/MainActivity.java
@@ -1,41 +1,72 @@
 package com.micutu.trafictube;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.micutu.trafictube.Crawler.TopVideosSingleton;
 import com.micutu.trafictube.Crawler.VideoListResponse;
-import com.micutu.trafictube.Crawler.VideosListResponse;
 import com.micutu.trafictube.Data.Video;
 
-import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
-
-    private RecyclerView mRecyclerView;
-    private MainRecyclerAdapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
 
-
-        TopVideosSingleton.getTwoDaysTopVideos(getApplicationContext(), new VideosListResponse() {
+        TopVideosSingleton.getVideoOfTheDay(getApplicationContext(), new VideoListResponse() {
             @Override
-            public void onResponse(List<Video> videos, Map<String, Object> extra) {
-                if(extra.containsKey("error")) {
+            public void onResponse(Video video, Map<String, Object> extra) {
+                if (extra.containsKey("error")) {
                     System.out.println(extra.get("error"));
                     return;
                 }
 
-                for(Video video : videos) {
-                    System.out.println(video);
+                System.out.println(video);
+            }
+        });
+    }
+
+    public void init() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
+
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.isChecked()) {
+                    item.setChecked(false);
+                } else {
+                    item.setChecked(true);
                 }
+
+                drawerLayout.closeDrawers();
+
+                return false;
+            }
+        });
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
             }
         });
     }
